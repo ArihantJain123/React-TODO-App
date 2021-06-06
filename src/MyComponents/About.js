f34063fdc5dbc8f67c7fb59965af6dd96c6886ac
ArihantJain123 <arihantjain2708@gmail.com>
2021-06-06 20:41:18 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>This is my first react app this project name is To do List</p>
+        </div>
+    )
+}
